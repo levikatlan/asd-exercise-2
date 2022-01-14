be3996ad29be756460af1ff7a4981e82eaab76a3
levi <levi@local-dev.com>
2022-01-14 02:58:58 -0800

    implement indenting trace decorator with printing argument

--- q1/e2d.py
+++ q1/e2d.py
@@ -1,2 +1,32 @@
 def trace(log):
-    pass # TODO
+    """receive the printing function as an argument"""
+    padding_length = 0
+
+    def decorator(f):
+        def format_args(args, kwargs):
+            str_kwargs = ", ".join((f"{k}={v}" for k, v in kwargs.items()))
+            str_args = ", ".join(map(str, args))
+            has_comma = (str_kwargs and ", ") or ''
+            return str_args + has_comma + str_kwargs
+
+        def wrapper(*args, **kwargs):
+            nonlocal padding_length
+            log(f'{" " * padding_length}enter {f.__name__}({format_args(args, kwargs)})\n')
+            padding_length += 1
+            result = f(*args, **kwargs)
+            padding_length -= 1
+            log(f'{" " * padding_length}leave {f.__name__}({format_args(args, kwargs)}): {result}\n')
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+@trace(print)
+def fib(n):
+    return n if n < 2 else fib(n - 1) + fib(n - 2)
+
+
+if __name__ == '__main__':
+    print(fib(3))
