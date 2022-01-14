1fbeb5a4d6e5a1ba5b46abcb2d85540fcaa8306e
levi <levi@local-dev.com>
2022-01-13 23:19:18 -0800

    implement is_prime function

--- q1/e1c.py
+++ q1/e1c.py
@@ -1,2 +1,13 @@
-def is_prime(p):
-    pass # TODO
+def is_prime(p: int) -> bool:
+    """
+    Return whether a number is prime
+    :param p: int - the number to validate
+    :return: boolean - True if the number is prime, False otherwise
+    """
+
+    return not any(i for i in range(2, p) if p % i == 0)
+
+
+if __name__ == '__main__':
+    print(is_prime(10))
+    print(is_prime(13))
