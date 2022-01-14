24e09fdd2778a84a16723d9c9ec369b390a6eb7c
levi <levi@local-dev.com>
2022-01-13 23:24:28 -0800

    implement trace decorator

--- q1/e2a.py
+++ q1/e2a.py
@@ -1,2 +1,19 @@
 def trace(f):
-    pass # TODO
+    """Traces whenever a function is invoked and whenever it returns"""
+
+    def wrapper(*args, **kwrags):
+        print(f'enter {f.__name__}')
+        result = f(*args, **kwrags)
+        print(f'leave {f.__name__}')
+        return result
+
+    return wrapper
+
+
+@trace
+def inc(x):
+    return x + 1
+
+
+if __name__ == '__main__':
+    print(inc(1))
