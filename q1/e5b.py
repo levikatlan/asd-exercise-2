b2c7f43b2d258679baedf4f59e0d7399f9d63ffe
levi <levi@local-dev.com>
2022-01-14 12:51:28 -0800

    implemet semi-parametrized exception_safa decorator

--- q1/e5b.py
+++ q1/e5b.py
@@ -1,2 +1,30 @@
+import functools
+import inspect
+
+
 def exception_safe(*args):
-    pass # TODO
+    if inspect.isfunction(args[0]):
+        print('no params passed')
+        f = args[0]
+
+    def decorator(f):
+        @functools.wraps(f)
+        def wrapper(*fargs, **fkwargs):
+            try:
+                return f(*fargs, **fkwargs)
+            except args:
+                print('no error')
+
+        return wrapper
+
+    return decorator
+
+
+@exception_safe(ValueError, NameError)
+def err(error):
+    raise error
+
+
+if __name__ == '__main__':
+    err(ValueError)
+    err(TypeError)
