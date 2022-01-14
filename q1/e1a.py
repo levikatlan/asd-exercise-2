6384539c35bf4b13e521642cf1bee480fe1ae3dc
levi <levi@local-dev.com>
2022-01-13 23:01:26 -0800

    implement file_sizes function

--- q1/e1a.py
+++ q1/e1a.py
@@ -1,2 +1,11 @@
+import pathlib
+
+
 def file_sizes():
-    pass # TODO
+    """Returns all the files mapped to their sizes in the current directory"""
+
+    return {path.name: path.stat().st_size for path in filter(pathlib.Path, pathlib.Path('.').iterdir())}
+
+
+if __name__ == '__main__':
+    print(file_sizes())
