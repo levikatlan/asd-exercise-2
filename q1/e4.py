cd146fa60fa369ddf56ce1c5ae0f1bd36ef52f01
levi <levi@local-dev.com>
2022-01-14 12:07:15 -0800

    implemet time_execution decorator

--- q1/e4.py
+++ q1/e4.py
@@ -1,2 +1,22 @@
+import time
+
+
 def time_execution(f):
-    pass # TODO
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = f(*args, **kwargs)
+        end = time.perf_counter()
+        print(f'{f.__name__} took {end - start:.2f} seconds to execute')
+        return result
+
+    return wrapper
+
+
+@time_execution
+def wait(n):
+    time.sleep(n)
+
+
+if __name__ == '__main__':
+    wait(1)
+    wait(2)
