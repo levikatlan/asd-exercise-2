53b7764ab1a6cbbe29cb3633db680a2de884ef42
levi <levi@local-dev.com>
2022-01-14 08:36:35 -0800

    implement cache atribute decorator

--- q1/e3b.py
+++ q1/e3b.py
@@ -1,2 +1,29 @@
+import functools
+
+
 def cache(f):
-    pass # TODO
+    """Exposes cache as the cache attribute of a function"""
+    f.cache = {}
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        if args in f.cache:
+            return f.cache[args]
+        res = f(*args, **kwargs)
+        f.cache[args] = res
+        return res
+
+    return wrapper
+
+
+@cache
+def fib(n):
+    print(f'computing fib({n})...')
+    return n if n < 2 else fib(n - 1) + fib(n - 2)
+
+
+if __name__ == '__main__':
+    print(fib(3))
+    print(fib.cache)
+    print(fib(5))
+    print(fib.cache)
