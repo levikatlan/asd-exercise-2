4303bfffae21ee695159022f9642527d0b04243a
levi <levi@local-dev.com>
2022-01-14 08:26:42 -0800

    implement cache decorator

--- q1/e3a.py
+++ q1/e3a.py
@@ -1,2 +1,23 @@
 def cache(f):
-    pass # TODO
+    """Caches function results by argument"""
+    cache = {}
+
+    def wrapper(*args, **kwargs):
+        if args in cache:
+            return cache[args]
+        res = f(*args, **kwargs)
+        cache[args] = res
+        return res
+
+    return wrapper
+
+
+@cache
+def fib(n):
+    print(f'computing fib({n})...')
+    return n if n < 2 else fib(n - 1) + fib(n - 2)
+
+
+if __name__ == '__main__':
+    print(fib(3))
+    print(fib(5))
