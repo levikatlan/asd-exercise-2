9367711cff58d80acdd526e0485b0dc2b145d07d
levi <levi@local-dev.com>
2022-01-13 23:12:24 -0800

    implement sieve_of_erathosthenes function

--- q1/e1b.py
+++ q1/e1b.py
@@ -1,2 +1,12 @@
-def sieve_of_eratosthenes(n):
-    pass # TODO
+def sieve_of_eratosthenes(n: int):
+    """
+    returns a set of all the primes up to a certain number
+
+    :param n: the given number up to which to get all the primes
+    :return: a set of all the primes up to n
+    """
+    return {i for i in range(2, n) if 0 not in [i % j for j in range(2, i)]}
+
+
+if __name__ == '__main__':
+    print(sieve_of_eratosthenes(100))
