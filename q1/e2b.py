d85aed5e6f586c27a66edf38a3c0c03c546ff4a6
levi <levi@local-dev.com>
2022-01-14 00:13:50 -0800

    implement extended trace decorator

--- q1/e2b.py
+++ q1/e2b.py
@@ -1,2 +1,31 @@
 def trace(f):
-    pass # TODO
+    """Reports arguments, returns values and errors"""
+
+    def format_args(args, kwargs):
+        str_kwargs = ", ".join((f"{k}={v}" for k, v in kwargs.items()))
+        str_args = ", ".join(map(str, args))
+        has_comma = (str_kwargs and ", ") or ''
+        return str_args + has_comma + str_kwargs
+
+    def wrapper(*args, **kwargs):
+        try:
+            print(f'enter {f.__name__}({format_args(args, kwargs)})')
+            result = f(*args, **kwargs)
+            print(f'leave {f.__name__}({format_args(args, kwargs)}): {result}')
+        except Exception as error:
+            print(f'leave {f.__name__}({format_args(args, kwargs)}):on error {error}')
+            result = error
+        return result
+
+    return wrapper
+
+
+@trace
+def div(x, y):
+    return x / y
+
+
+if __name__ == '__main__':
+    print(div(4, 2))
+    print(div(x=1, y=0))
+    print(div(1, y=2))
