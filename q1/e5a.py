a2b5e7c4c5c820252dd99f175bd245b2a7e7865a
levi <levi@local-dev.com>
2022-01-14 12:30:50 -0800

    implemet exception_safa decorator

--- q1/e5a.py
+++ q1/e5a.py
@@ -1,2 +1,24 @@
+import functools
+
+
 def exception_safe(f):
-    pass # TODO
+    """Makes a function exception-safe by suppressing them"""
+
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        # noinspection PyBroadException
+        try:
+            return f(*args, **kwargs)
+        except Exception:
+            pass
+
+    return wrapper
+
+
+@exception_safe
+def div(x, y):
+    return x / y
+
+
+if __name__ == '__main__':
+    div(1, 0)
