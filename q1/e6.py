e2504303e6fa2dbb6aede0bb31efa6796557c88a
levi <levi@local-dev.com>
2022-01-15 05:19:53 -0800

    implemet synchronize decorator for thread safe actions

--- q1/e6.py
+++ q1/e6.py
@@ -1,2 +1,36 @@
+import threading
+import time
+
+
 def synchronize(lock):
-    pass # TODO
+    """Synchronizes function with respect to some lock"""
+
+    def decorator(f):
+        def wrapper(*args, **kwargs):
+            with lock:
+                result = f(*args, **kwargs)
+            return result
+
+        return wrapper
+
+    return decorator
+
+
+lock = threading.Lock()
+
+
+@synchronize(lock=lock)
+def f():
+    time.sleep(1)
+    print('f')
+
+
+@synchronize(lock=lock)
+def g():
+    time.sleep(1)
+    print('g')
+
+
+if __name__ == '__main__':
+    threading.Thread(target=f).start()
+    threading.Thread(target=g).start()
